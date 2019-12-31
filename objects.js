b740dfaa984a2b60e30e5e5a73ad34285cfb949e
Sekou Dosso <sekou.dosso82@gmail.com>
2019-12-31 16:12:25 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = {
   artistName : "Bob"
 }
 
-var updatePlaylist(playlist, artistName, songTitle) = {
+function updatePlaylist(playlist, artistName, songTitle) = {
   playlist[artistName] = songTitle
   return playlist
 }
