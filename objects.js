944280e84190bb9e5a1fd32b1ed38eb24a537117
Sekou Dosso <sekou.dosso82@gmail.com>
2019-12-31 16:15:02 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -8,6 +8,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName){
-  playlist.remove(artistName)
+  delete playlist.artistName
   return playlist
 }
